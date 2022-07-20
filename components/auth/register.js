7f9b9583e2f4b3c62943e75019525e283ab6cc79
devwasik <devwasik@gmail.com>
2022-07-19 22:27:17 -0500

    Update register.js

--- components/auth/register.js
+++ components/auth/register.js
@@ -3,13 +3,22 @@ import axios from "axios";
 import { useFormik } from "formik";
 
 export default function RegistrationForm(props) {
-  const validate = values => {
+  const validate = (values) => {
     const errors = {};
 
+    console.log(values)
     if (!values.email) {
-      errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+    if (!values.password) {
+        errors.password = "Required";
+    }
+    if (values.password != values.passwordConfirmation) {
+        errors.passwordConfirmation = "Passwords do not match";
     }
     return errors;
   };
@@ -43,19 +52,43 @@ export default function RegistrationForm(props) {
                 onChange={formik.handleChange}
                 value={formik.values.email}
               />
-               {formik.errors.email ? <div style={{color:"red"}}>{formik.errors.email}</div> : null}
+              {formik.errors.email ? (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+              ) : null}
             </div>
             <div class="form-control">
               <label class="label">
                 <span class="label-text">Password</span>
               </label>
-              <input type="password" class="input input-bordered" />
+              <input
+                type="password"
+                class="input input-bordered"
+                id="password"
+                name="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <div style={{ color: "red" }}>{formik.errors.password}</div>
+              ) : null}
             </div>
             <div class="form-control">
               <label class="label">
                 <span class="label-text">Confirm Password</span>
               </label>
-              <input type="password" class="input input-bordered" />
+              <input
+                type="password"
+                class="input input-bordered"
+                id="passwordConfirmation"
+                name="passwordConfirmation"
+                onChange={formik.handleChange}
+                value={formik.values.passwordConfirmation}
+              />
+              {formik.touched.passwordConfirmation && formik.errors.passwordConfirmation  ? (
+                <div style={{ color: "red" }}>
+                  {formik.errors.passwordConfirmation}
+                </div>
+              ) : null}
             </div>
             <div class="form-control mt-6">
               <button class="btn btn-primary">Register</button>
