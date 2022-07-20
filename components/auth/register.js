021f3a603fe7753a8f0c32b3c19632f726f9d496
devwasik <devwasik@gmail.com>
2022-07-19 22:16:09 -0500

    Update register.js

--- components/auth/register.js
+++ components/auth/register.js
@@ -1,48 +1,68 @@
-import {useState} from 'react'
-import axios from 'axios'
-import { useFormik } from 'formik';
+import { useState } from "react";
+import axios from "axios";
+import { useFormik } from "formik";
 
 export default function RegistrationForm(props) {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  
+  const validate = values => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div class="hero min-h-screen bg-base-200">
-      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <div class="card-body">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="text"
-              class="input input-bordered"
-            />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              class="input input-bordered"
-            />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Confirm Password</span>
-            </label>
-            <input
-              type="password"
-              class="input input-bordered"
-            />
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-primary">Register</button>
+    <form onSubmit={formik.handleSubmit}>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div class="card-body">
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                class="input input-bordered"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+               {formik.errors.email ? <div style={{color:"red"}}>{formik.errors.email}</div> : null}
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input type="password" class="input input-bordered" />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Confirm Password</span>
+              </label>
+              <input type="password" class="input input-bordered" />
+            </div>
+            <div class="form-control mt-6">
+              <button class="btn btn-primary">Register</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
