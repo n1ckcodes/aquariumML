3e62c3855563351b5bd91a7ecc75fd6c4f4e2cf7
Nick <nickwasik@gmail.com>
2022-08-24 22:10:33 -0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header(props) {
   return (
-    <div class='grid grid-cols-12 col-span-12 h-20 flex'>
+    <div class='grid grid-cols-12 col-span-12 h-20  bg-neutralflex'>
         <div class="col-span-11 flex pl-10">
       <Link href="/">
         <a class="flex items-center text-2xl">
