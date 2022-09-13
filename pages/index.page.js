e0d59835921c3818e475efacc720eb89e77dddb6
Nick <nickwasik@gmail.com>
2022-09-12 21:59:12 -0500

    Update index.page.js

--- pages/index.page.js
+++ pages/index.page.js
@@ -33,6 +33,31 @@ const mockArticles = [
     title: "article 6",
     blurb: "fresh water aquarium plants",
   },
+  {
+    title: "article 1",
+    blurb: "fresh water aquarium test",
+    tags: "freshwater, testing",
+  },
+  {
+    title: "article 2",
+    blurb: "fresh water aquarium beginnners",
+  },
+  {
+    title: "article 3",
+    blurb: "fresh water aquarium planted tank",
+  },
+  {
+    title: "article 4",
+    blurb: "fresh water aquarium community tank",
+  },
+  {
+    title: "article 5",
+    blurb: "fresh water aquarium cichlids",
+  },
+  {
+    title: "article 6",
+    blurb: "fresh water aquarium plants",
+  },
 ];
 
 export const getServerSideProps = withSessionSsr(
@@ -78,7 +103,7 @@ export default function Home({ user }) {
     );
   };
   return (
-    <html data-theme="night" class="flex flex-col min-h-screen">
+    <html>
       {renderController()}
     </html>
   );
