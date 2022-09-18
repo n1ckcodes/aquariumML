907e115e897daf8c6f5974519c67344e64e46af1
Nick <devwasik@gmail.com>
2022-09-18 14:19:06 -0500

    -

--- components/layouts/dashboard/dashboardNav.js
+++ components/layouts/dashboard/dashboardNav.js
@@ -2,29 +2,24 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import AquariumIcon from "../../../public/images/aquariumMenuIcon.svg";
-import FishIcon from '../../../public/images/fish.svg'
-import CalendarIcon from '../../../public/images/calendar.svg'
+import FishIcon from "../../../public/images/fish.svg";
+import CalendarIcon from "../../../public/images/calendar.svg";
 export default function DashboardNav(props) {
-
   const { pathname } = useRouter();
   return (
-    <nav style={{background:"#D3D3D3", height:"100vh"}}>
-    <ul>
-      <Link href="#" >
-        <a >
-          <div style={{paddingBottom:"2vh", paddingTop:"2vh"}}>
-          <li >
-              Home
-          </li>
-          </div>
-        </a>
-      </Link>
-      <Link href="#">
-        <a>
-        <div style={{paddingBottom:"2vh"}}>
-          <li>
-          
-              {/* <Image
+    <nav style={{ background: "#D3D3D3", height: "100vh" }}>
+      <ul>
+        <Link href="#">
+          <a>
+            <div style={{ paddingBottom: "2vh", paddingTop: "2vh" }}>
+              <li>Home</li>
+            </div>
+          </a>
+        </Link>
+
+        <div style={{ paddingBottom: "2vh" }}>
+          <h5 style={{textAlign:"center", textDecoration:"underline"}}>
+            {/* <Image
                 class="inline-block"
                 alt="Mountains"
                 src={AquariumIcon}
@@ -32,27 +27,39 @@ export default function DashboardNav(props) {
                 width={700}
                 height={475}
               />{" "} */}
-              Tanks
-          </li>
-          </div>
-        </a>
-      </Link>
+            Tanks
+          </h5>
+          <li><Link href="/ml/tanks/add">Add</Link></li>
+          <li>Edit</li>
+          <li>View</li>
+        
+          <li>Delete</li>
+        </div>
 
-      <Link href="#">
-        <a>
-        <div style={{paddingBottom:"2vh"}}>
-          <li >
-         
-              Maintenance
-          </li>
-          </div>
-        </a>
-      </Link>
-      <Link href="#">
-        <a>
-        <div style={{paddingBottom:"2vh"}}>
-          <li >
-          {/* <Image
+        <div style={{ paddingBottom: "2vh" }}>
+          <h5 style={{textAlign:"center", textDecoration:"underline"}}>
+            {/* <Image
+                class="inline-block"
+                alt="Mountains"
+                src={AquariumIcon}
+                layout="responsive"
+                width={700}
+                height={475}
+              />{" "} */}
+            Maintenance
+          </h5>
+          <li><Link href="#">Add</Link></li>
+          <li><Link href="#">Schedules</Link></li>
+          <li>Edit</li>
+          <li>View</li>
+        
+          <li>Delete</li>
+        </div>
+        <Link href="#">
+          <a>
+            <div style={{ paddingBottom: "2vh" }}>
+              <li>
+                {/* <Image
             class="inline-block"
             alt="Mountains"
             src={FishIcon}
@@ -60,16 +67,16 @@ export default function DashboardNav(props) {
             width={700}
             height={475}
           />{" "} */}
-              Fish
-          </li>
-          </div>
-        </a>
-      </Link>
-      <Link href="#">
-        <a>
-        <div style={{paddingBottom:"2vh"}}>
-          <li>
-            {/* <Image
+                Fish
+              </li>
+            </div>
+          </a>
+        </Link>
+        <Link href="#">
+          <a>
+            <div style={{ paddingBottom: "2vh" }}>
+              <li>
+                {/* <Image
             class="inline-block"
             alt="Mountains"
             src={CalendarIcon}
@@ -77,12 +84,12 @@ export default function DashboardNav(props) {
             width={700}
             height={475}
           />{" "} */}
-              Calendar
-          </li>
-          </div>
-        </a>
-      </Link>
-    </ul>
+                Calendar
+              </li>
+            </div>
+          </a>
+        </Link>
+      </ul>
     </nav>
   );
 }
--- pages/ml/tanks/add.page.js
+++ pages/ml/tanks/add.page.js
@@ -1,8 +1,14 @@
 import { withSessionSsr } from "../../../helpers/ironSession";
+import React, { useState } from "react";
 import Layout from "../../../components/layouts/master";
-import MaintenanceDashboard from '../../../components/layouts/maintenanceDashboard'
-import Container from 'react-bootstrap/Container';
-
+import MaintenanceDashboard from "../../../components/layouts/maintenanceDashboard";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 export const getServerSideProps = withSessionSsr(
   async function getServerSideProps({ req }) {
@@ -23,11 +29,90 @@ export const getServerSideProps = withSessionSsr(
 );
 
 export default function AddTank({ user }) {
+  const radios = [
+    { name: "Gallons", value: "gallons" },
+    { name: "Liters", value: "liters" },
+  ];
 
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState("1");
 
   return (
-  <MaintenanceDashboard user={user}>
- add your tank here
-  </MaintenanceDashboard>
+    <MaintenanceDashboard user={user}>
+      <Container>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Tank Name
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="text"
+                placeholder="Tank 1, Office Tank, Johns Aquarium, etc..."
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalPassword"
+          >
+            <Form.Label column sm={2}>
+              Tank Size
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="number"
+                placeholder="Size of tank in liters or gallons"
+              />
+              <ButtonGroup className="mb-2">
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={
+                      radioValue == radio.value
+                        ? "outline-primary"
+                        : "outline-secondary"
+                    }
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Start Date
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="date"
+                placeholder="When the tank was started"
+              />
+            </Col>
+          </Form.Group>
+      
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Form.Check label="Remember me" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </Container>
+    </MaintenanceDashboard>
   );
 }
