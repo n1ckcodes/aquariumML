27f6f5e24160ceae1fd114662adcead9d368a7c6
Nick <nickwasik@gmail.com>
2022-09-21 13:51:03 -0500

    -

--- pages/index.page.js
+++ pages/index.page.js
@@ -33,7 +33,7 @@ export default function Home({ user }) {
   const renderController = () => {
     return (
        <Layout user={user}>
-          <Row>
+          <Row className="row1">
            
               <div style={{color:"white", textAlign:"center",  marginTop:"20vh", marginBottom:"2vh"}}>
               <span className="hero1" style={{color:"white", fontSize:"4em", textAlign:"center"}}>Speak the language of aquarists.</span>
@@ -51,7 +51,7 @@ export default function Home({ user }) {
             <Row>
               <Col lg={3} xl={3}></Col>
               <Col lg={6} xl={6}><br />
-            {articles.map((a) => {
+            {/* {articles.map((a) => {
                 return (
                   <Col sm={2}md={2} lg={5} xl={4} style={{margin:"2", display:"inline-block" }}>
                     <ArticleCard 
@@ -62,7 +62,7 @@ export default function Home({ user }) {
                     />
                   </Col>
                 );
-              })}
+              })} */}
               </Col>
               <Col lg={3} xl={3}></Col>
               </Row>
@@ -89,6 +89,118 @@ export default function Home({ user }) {
                 src={"./images/hero6.jpg"}
               /> */}
           </Row>
+          <Row className="row2">
+           
+           <div style={{color:"white", textAlign:"center",  marginTop:"20vh", marginBottom:"2vh"}}>
+           <span className="hero1" style={{color:"white", fontSize:"4em", textAlign:"center"}}>Speak the language of aquarists.</span>
+           <br/><br />
+           AquariumQL is a comprehensive platform for aquarium guides, knowledge sharing, and managing your aquarium maintence schedule </div>
+           <br /><br /><br />
+           <Col lg={3} xl={3}></Col>
+           <Col lg={6} xl={6} style={{textAlign:"center"}}> 
+           <Button variant="secondary" style={{margin:"1vh"}} size="lg">Get Started</Button>
+           <Button variant="secondary" size="lg"  style={{margin:"1vh"}}>
+       Learn more
+     </Button>
+     </Col>
+     <Col lg={3} xl={3}></Col>
+         <Row>
+           <Col lg={3} xl={3}></Col>
+           <Col lg={6} xl={6}><br />
+         {/* {articles.map((a) => {
+             return (
+               <Col sm={2}md={2} lg={5} xl={4} style={{margin:"2", display:"inline-block" }}>
+                 <ArticleCard 
+
+                   header={a.header}
+                   title={a.title}
+                   blurb={a.blurb}
+                 />
+               </Col>
+             );
+           })} */}
+           </Col>
+           <Col lg={3} xl={3}></Col>
+           </Row>
+           {" "}
+   
+           {/* <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero7.jpg"}
+           />
+           <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero5.jpg"}
+           />{" "}
+           <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero4.jpg"}
+           />
+           <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero6.jpg"}
+           /> */}
+       </Row>
+       <Row className="row3">
+           
+           <div style={{color:"white", textAlign:"center",  marginTop:"20vh", marginBottom:"2vh"}}>
+           <span className="hero1" style={{color:"white", fontSize:"4em", textAlign:"center"}}>Speak the language of aquarists.</span>
+           <br/><br />
+           AquariumQL is a comprehensive platform for aquarium guides, knowledge sharing, and managing your aquarium maintence schedule </div>
+           <br /><br /><br />
+           <Col lg={3} xl={3}></Col>
+           <Col lg={6} xl={6} style={{textAlign:"center"}}> 
+           <Button variant="secondary" style={{margin:"1vh"}} size="lg">Get Started</Button>
+           <Button variant="secondary" size="lg"  style={{margin:"1vh"}}>
+       Learn more
+     </Button>
+     </Col>
+     <Col lg={3} xl={3}></Col>
+         <Row>
+           <Col lg={3} xl={3}></Col>
+           <Col lg={6} xl={6}><br />
+         {/* {articles.map((a) => {
+             return (
+               <Col sm={2}md={2} lg={5} xl={4} style={{margin:"2", display:"inline-block" }}>
+                 <ArticleCard 
+
+                   header={a.header}
+                   title={a.title}
+                   blurb={a.blurb}
+                 />
+               </Col>
+             );
+           })} */}
+           </Col>
+           <Col lg={3} xl={3}></Col>
+           </Row>
+           {" "}
+   
+           {/* <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero7.jpg"}
+           />
+           <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero5.jpg"}
+           />{" "}
+           <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero4.jpg"}
+           />
+           <img
+             className="img-fluid "
+             alt="Vercel logo"
+             src={"./images/hero6.jpg"}
+           /> */}
+       </Row>
        </Layout>
     );
   };
