5f9a4f699a48bcf85cf03d8a1d5b0c43bda34e37
Nick <devwasik@gmail.com>
2022-11-21 15:33:28 -0600

    fix wierd resize bug on mobile

--- components/layouts/dashboard/MobileDashboardNav.js
+++ components/layouts/dashboard/MobileDashboardNav.js
@@ -4,19 +4,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 export default function MobileDashboardNav(props) {
   const { pathname } = useRouter();
-  console.log('here')
+  console.log("here");
   return (
     <Navbar bg="primary" fixed="bottom" variant="dark" className="py-4">
-      <Container fluid style={{textAlign:"center", color:"white"}}>
-
-          <Col xs={3}><FontAwesomeIcon icon="home" /> </Col>
-          <Col xs={3}><FontAwesomeIcon icon="fish" /></Col>
-          <Col xs={3}><FontAwesomeIcon icon="tools" /></Col> 
-          <Col xs={3}> <FontAwesomeIcon icon="gear" /></Col>
+      <Container style={{ textAlign: "center", color: "white" }}>
+        <Col xs={3}>
+          <a href="/dashboard">
+            <FontAwesomeIcon icon="home" />
+          </a>
+        </Col>
+        <Col xs={3}>
+          <a href="/dashboard/tanks">
+            <FontAwesomeIcon icon="fish" />
+          </a>
+        </Col>
+        <Col xs={3}>
+          <a href="#">
+            <FontAwesomeIcon icon="tools" />
+          </a>
+        </Col>
+        <Col xs={3}>
+          <a href="#">
+            <FontAwesomeIcon icon="gear" />
+          </a>
+        </Col>
       </Container>
     </Navbar>
   );
