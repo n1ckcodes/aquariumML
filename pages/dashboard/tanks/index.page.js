fd797331459a4f3072aa9367a64a5ea37c7ff786
Nick <devwasik@gmail.com>
2022-11-22 12:53:35 -0600

    Update index.page.js

--- pages/dashboard/tanks/index.page.js
+++ pages/dashboard/tanks/index.page.js
@@ -1,7 +1,7 @@
 import MaintenanceDashboard from "components/layouts/dashboard/DashboardLayout";
 import { withSessionSsr } from "helpers/ironSession";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col"
+import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import MockTankData from "data/mockTanks.json";
 import TankCard from "components/dashboard/TankCard";
@@ -35,27 +35,42 @@ export default function Tanks({ user }) {
   const renderController = () => {
     return (
       <MaintenanceDashboard>
-        <Row style={{paddingTop:"4vh"}}>
-        <Col xs={6}>
-          <div>
-            <FontAwesomeIcon style={{fontSize:"3em", color:"gray"}} icon="filter" onClick={handleShow} />
-            Sort by
-          </div>
+        <Row style={{ paddingTop: "4vh" }}>
+          <Col xs={6}>
+            <div>
+              <FontAwesomeIcon
+                style={{ fontSize: "3em", color: "gray" }}
+                icon="filter"
+                onClick={handleShow}
+              />
+              Sort by
+            </div>
           </Col>
           <Col xs={6}>
-          <div style={{ float: "right" }}>
-            <FontAwesomeIcon style={{fontSize:"3em", color:"green"}} icon="square-plus" onClick={handleShow} />
-            
-          </div>
+            <div style={{ float: "right" }}>
+              <FontAwesomeIcon
+                style={{ fontSize: "3em", color: "green" }}
+                icon="square-plus"
+                onClick={handleShow}
+              />
+            </div>
           </Col>
-
         </Row>
-        <Row style={{paddingTop:"4vh", margin: "0 auto" }}>
+        <Row style={{ paddingTop: "4vh", margin: "0 auto" }}>
           {MockTankData.map((tank) => {
             return (
-              <div style={{ display:"flex", justifyContent:"center",width: "100%", marginBottom: "4vh"  }}>
-                <TankCard tank={tank} />
-              </div>
+              <Col xs={12} sm={6} md={4}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100%",
+                    marginBottom: "4vh",
+                  }}
+                >
+                  <TankCard tank={tank} />
+                </div>
+              </Col>
             );
           })}
         </Row>
@@ -65,9 +80,11 @@ export default function Tanks({ user }) {
           onHide={handleClose}
           aria-labelledby="contained-modal-title-vcenter"
           centered
+          backdrop="static"
+          keyboard={false}
         >
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Add Tank</Modal.Title>
           </Modal.Header>
           <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
           <Modal.Footer></Modal.Footer>
