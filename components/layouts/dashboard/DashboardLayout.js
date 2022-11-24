7432235fd3b6a4e2732ea867dd37d723a556af7f
Nick <devwasik@gmail.com>
2022-11-23 20:25:58 -0600

    Update DashboardLayout.js

--- components/layouts/dashboard/DashboardLayout.js
+++ components/layouts/dashboard/DashboardLayout.js
@@ -5,25 +5,45 @@ import Col from "react-bootstrap/Col";
 import { Container } from "react-bootstrap";
 import DesktopDashboardNav from "./DesktopDashboardNav";
 import MobileDashboardNav from "./MobileDashboardNav";
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect";
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 export default function MaintenanceDashboard({ children, user }) {
+  const [state, setState] = useState({
+    mobileView: false,
+  });
+
+  const { mobileView } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 600
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
   return (
     <Layout>
-      {isMobile ? <MobileDashboardNav /> : <></>}
+         {mobileView ? <MobileDashboardNav /> : <></>}
       <Row style={{ height: "100%" }}>
-        {isBrowser ? (
+   
+        {!mobileView ? 
           <Col sm={2} md={3} lg={4} xl={1}>
             <DesktopDashboardNav />
           </Col>
-        ) : null}
+         : null }
 
         <Col>{children}</Col>
+   
       </Row>
+
     </Layout>
   );
 }
